148710ea8fc4f3a7fa64c687d8002f27673a0084
kmlhtpgl <kemaledu@gmail.com>
2021-08-09 18:50:23 +0000

    app repo name changed

--- jenkins/build-and-deploy-petclinic-on-qa-env-manually.sh
+++ jenkins/build-and-deploy-petclinic-on-qa-env-manually.sh
@@ -1,6 +1,6 @@
 PATH="$PATH:/usr/local/bin"
 APP_NAME="petclinic"
-APP_REPO_NAME="clarusway-repo/petclinic-app-qa"
+APP_REPO_NAME="clarusway2-repo/petclinic-app-qa"
 APP_STACK_NAME="Kemal-petclinic-App-QA-3"
 CFN_KEYPAIR="kemal-petclinic-qa.key"
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
